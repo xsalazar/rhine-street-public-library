6d2f702aaa0f5d251b57710e71a71eb7fa412b18
Xavier Salazar <x.salazar1729@gmail.com>
2023-07-29 19:11:54 -0700

    Request data from backend on load

--- src/Components/body.tsx
+++ src/Components/body.tsx
@@ -1,12 +1,35 @@
 import { Container, ListItemButton, ImageList } from "@mui/material";
+import axios from "axios";
 
 import React from "react";
-import data from "./../data/books.json";
 import BookCard from "./bookCard";
+import { Book } from "./types";
 
-export default class Body extends React.Component {
+interface BodyProps {}
+
+interface BodyState {
+  books: Array<Book>;
+}
+
+export default class Body extends React.Component<BodyProps, BodyState> {
+  constructor(props: BodyProps) {
+    super(props);
+    this.state = { books: [] };
+  }
+
+  async componentDidMount(): Promise<void> {
+    const result = (
+      await axios.get(`https://backend.rhinestreetpubliclibrary.com/`)
+    ).data;
+
+    this.setState({
+      books: result.books,
+    });
+  }
+
   render() {
-    const sortedBooks = data.books.sort((a, b) => (b.available ? 1 : -1));
+    const { books } = this.state;
+    const sortedBooks = books.sort((a, b) => (b.available ? 1 : -1));
 
     return (
       <Container
